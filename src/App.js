b07d570da5a06d8d98468127fb1fb180261740df
jaypatel <jaypatel9800@gmail.com>
2021-09-13 13:29:09 +0530

    convert to pwa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MainPage from "./components/mainpage/MainPage";
+import Skill from "./components/skills/Skills";
+import About from "./components/aboutpage/About";
+import Projects from "./components/Projects/Projects";
+import Back from "./assets/back.png";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="MainDiv">
+        <Switch>
+          <Route path="/" exact>
+            <MainPage />
+          </Route>
+          <Route path="/skill">
+            <Skill />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <img src={Back} alt="back" className="mainpage__backImg" />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
